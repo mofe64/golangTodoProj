757944cea8f89b9088d02b31a0fed3530be81620
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-01 14:55:07 +0100

    "cors debug"

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -1,9 +1,13 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		log.Println("cors method", c.Request.Method)
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
