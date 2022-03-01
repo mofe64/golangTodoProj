370859a3bb0479197cb262fa37f195520544c9b3
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-01 16:32:47 +0100

    "UPDATED CORS CONFIG"

--- routes/task_route.go
+++ routes/task_route.go
@@ -9,7 +9,7 @@ import (
 func TaskRoute(router *gin.Engine) {
 	taskRoutes := router.Group("/tasks", middleware.AuthorizeJWT())
 	{
-		taskRoutes.POST("/", controller.CreateTask())
+		taskRoutes.POST("", controller.CreateTask())
 		taskRoutes.GET("/complete/:taskId", controller.CompleteTask())
 		taskRoutes.GET("/user/:userId", controller.GetAllMyTasks())
 		taskRoutes.GET("/user/:userId/today", controller.GetMyTaskForToday())
