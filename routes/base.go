3928a0a50a578b023fca5702a3484f4e9bd45c82
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-01 11:49:12 +0100

    "cors middleware impl"

--- routes/base.go
+++ routes/base.go
@@ -2,9 +2,10 @@ package routes
 
 import (
 	"accountability_back/controller"
+	"accountability_back/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func BaseRoute(router *gin.Engine) {
-	router.GET("/", controller.Ping())
+	router.GET("/", middleware.CORSMiddleware(), controller.Ping())
 }
--- routes/user_route.go
+++ routes/user_route.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"accountability_back/controller"
+	"accountability_back/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoute(router *gin.Engine) {
-	router.POST("/user/register", controller.CreateUser())
-	router.POST("/user/login", controller.Login())
-	router.GET("/user/:userId", controller.GetAUser())
+	userRoutes := router.Group("/user", middleware.CORSMiddleware())
+	{
+		userRoutes.POST("/register", controller.CreateUser())
+		userRoutes.POST("/login", controller.Login())
+		userRoutes.GET("/:userId", controller.GetAUser())
+	}
+
 }
