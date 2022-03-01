2c186e9e20c0f6ae143d33e7123ef166b5f47b50
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-01 17:06:28 +0100

    "UPDATED CORS CONFIG"

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -8,12 +8,14 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		log.Println("cors method", c.Request.Method)
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://accountabilityproject.herokuapp.com")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT,PATCH, OPTIONS, DELETE")
-
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		c.Next()
 	}
 }
