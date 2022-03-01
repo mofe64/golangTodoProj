5ae6bc1e98d0d9c89a2bfe5513b0cf2a86158306
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-01 16:01:11 +0100

    "allow options via cors"

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -13,11 +13,6 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
 		c.Next()
 	}
 }
