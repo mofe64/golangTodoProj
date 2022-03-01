883c6b7e11f942ecaff75704494415b50360311e
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-03-01 14:20:39 +0100

    "date addition big fix"

--- routes/task_route.go
+++ routes/task_route.go
@@ -10,7 +10,7 @@ func TaskRoute(router *gin.Engine) {
 	taskRoutes := router.Group("/tasks", middleware.AuthorizeJWT())
 	{
 		taskRoutes.POST("/", controller.CreateTask())
-		taskRoutes.GET("/user/:userId", controller.GetAllMyTasks())
+		taskRoutes.GET("/user/:userId/all", controller.GetAllMyTasks())
 		taskRoutes.GET("/user/:userId/today", controller.GetMyTaskForToday())
 		taskRoutes.DELETE("/:taskId", controller.DeleteTask())
 	}
